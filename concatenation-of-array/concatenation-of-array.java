680b564373e21ec061393509791e1caf5fce5260
YH.LEE <61870748+gutenLEE@users.noreply.github.com>
2021-08-16 10:15:17 +0900

    Time: 1 ms (63.02%), Space: 39.8 MB (35.34%) - LeetHub

---
+++ concatenation-of-array/concatenation-of-array.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        
+        // return array 'ans'
+        int[] ans = new int[nums.length * 2];
+        
+        for(int i = 0; i < nums.length; i++){
+            ans[i] = nums[i];
+            ans[i + nums.length] = nums[i];
+        }
+        return ans;
+    }
+}
