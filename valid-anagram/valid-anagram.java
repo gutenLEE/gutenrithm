2523d51247f98ab7f3ab784bc23730b5ff183246
YH.LEE <61870748+gutenLEE@users.noreply.github.com>
2021-12-24 09:19:18 +0900

    Time: 3 ms (77.46%), Space: 39.1 MB (86.15%) - LeetHub

--- valid-anagram/valid-anagram.java
+++ valid-anagram/valid-anagram.java
@@ -1,17 +1,19 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
-        int[] arr = new int[26];
         
+        if( s.length() != t.length() ) return false;
+        
+        int[] arr = new int[26];
         // 97 - 122
         for( int i = 0; i < s.length(); i++ ){
             int asciiValue = s.charAt(i);
             arr[ asciiValue - 97] += 1;
-        }
-        for( int i = 0; i < t.length(); i++ ){
+            
             int asciiValue2 = t.charAt(i);
             arr[ asciiValue2 - 97] -= 1;
         }
+
         
         boolean flag = true;
         for ( int elm : arr) {
@@ -21,5 +23,6 @@ class Solution {
             }
         }
         return flag;
+        
     }
 }
