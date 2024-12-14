f12a62ebf759be5a716a30f61fce61695b3c9280
youhee lee <61870748+gutenLEE@users.noreply.github.com>
2024-12-14 17:39:13 +0900

    Time: 45 ms (7.7%), Space: 17.1 MB (19.19%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        # iterate over array
+        # append each letter
+
+        min_length = min(len(word1), len(word2))
+        max_length = max(len(word1), len(word2))
+
+        answer = ''
+        for i in range(0, min_length):
+            answer += word1[i]
+            answer += word2[i]
+        
+        if len(word1) > len(word2):
+            answer += word1[min_length:]
+        else:
+            answer += word2[min_length:]
+        
+        return answer
+
+        
